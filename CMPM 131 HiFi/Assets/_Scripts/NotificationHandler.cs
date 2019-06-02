130b93d2ac41aa7ad336843d45873726d9816121
Grace <grko@ucsc.edu>
2019-06-01 17:07:52 -0700

    add notification scene

---
+++ CMPM 131 HiFi/Assets/_Scripts/NotificationHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NotificationHandler : MonoBehaviour
+{
+    public void MessageClick()
+    {
+        SceneManager.LoadScene("MessageScene");
+    }
+
+    public void SwapClick()
+    {
+        SceneManager.LoadScene("SwapScene");
+    }
+
+    public void CalendarClick()
+    {
+        SceneManager.LoadScene("CalendarScene");
+    }
+}
